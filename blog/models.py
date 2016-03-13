bd6675c60ae8804f49993d3aefeb4eed5a55da50
abhishek <abhishekreddy1206@gmail.com>
2016-03-13 15:10:21 -0700

    form added

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.CharField(max_length=600)
+    posted_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
